332eeb98f3da5b56b1d927ff1f40b57f54a8be9f
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-05-29 19:00:34 +0300

    Not related to PR. Add instance profile support.

--- iam.tf
+++ iam.tf
@@ -36,6 +36,11 @@ resource "aws_iam_policy" "ec2_assume_role" {
   policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
+resource "aws_iam_role_policy_attachment" "ec2_assume_role" {
+  role       = aws_iam_role.ec2_assume_role.name
+  policy_arn = aws_iam_policy.ec2_assume_role.arn
+}
+
 resource "aws_iam_instance_profile" "ec2_assume_role" {
   name = "${var.resource_name_prefix}-ec2-assume-role"
   role = aws_iam_role.ec2_assume_role.name
