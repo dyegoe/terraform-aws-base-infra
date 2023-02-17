d00a5d5f60469f785b989f1617536931fac0cd80
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2023-02-17 14:41:54 +0200

    Bump version

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.2.3"
+  required_version = "~> 1.3.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,7 +11,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.1.3"
+      version = "~> 3.1.3"
     }
   }
 }
