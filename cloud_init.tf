66ce1e26258956b8df94933ecb26a2140d17592a
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2023-05-28 00:07:59 +0300

    general refactor of the module closes #13 closes #14

---
+++ cloud_init.tf
@@ -0,0 +1,22 @@
+data "cloudinit_config" "ec2_instance" {
+  gzip          = false
+  base64_encode = true
+
+  part {
+    filename     = "set-hostname.sh"
+    content_type = "text/x-shellscript"
+
+    content = file("${path.module}/templates/set-hostname.sh")
+  }
+
+  part {
+    filename     = "cloud-config.yaml"
+    content_type = "text/cloud-config"
+
+    content = templatefile("${path.module}/templates/cloud-config.yaml",
+      {
+        ssh_port = var.ssh.port
+      }
+    )
+  }
+}
