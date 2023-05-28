0ebfa639e7df86998f2d0c5b1721620342ce9c3f
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2023-05-28 23:34:35 +0300

    change set-hostname script

--- templates/set-hostname.sh
+++ templates/set-hostname.sh
@@ -2,4 +2,7 @@
 
 URL="http://169.254.169.254/latest"
 TOKEN=`curl -s -X PUT "$URL/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-hostnamectl set-hostname $(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" $URL/meta-data/tags/instance/Name)
+NAME=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN" $URL/meta-data/tags/instance/Name`
+PROJECT=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN" $URL/meta-data/tags/instance/Project`
+DOMAIN=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN" $URL/meta-data/tags/instance/ZoneDomain`
+hostnamectl set-hostname $NAME.$PROJECT.$DOMAIN
